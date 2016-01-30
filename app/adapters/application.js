96ddea6c5547159ad605f09b0a285662c6269a16
Ian Nguyen <iannknguyen@gmail.com>
2016-01-29 18:49:56 -0800

    change ama

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 import ENV from 'ember-front/config/environment';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
-export default DS.RESTAdapter.extend(DataAdapterMixin, {
+export default DS.ActiveModelAdapter.extend(DataAdapterMixin, {
   host: ENV.APP.API_HOST,
   authorizer: 'authorizer:devise',
   // headers: Ember.computed(function() {
