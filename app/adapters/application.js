0f3b213ae7be0a721f1ef6ea2979b1528a8a6cc3
Ian Nguyen <iannknguyen@gmail.com>
2016-01-29 17:19:06 -0800

    add host url to adapter

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
 export default DS.RESTAdapter.extend(DataAdapterMixin, {
+  host: 'http://gospot-back.herokuapp.com',
   authorizer: 'authorizer:devise',
   // headers: Ember.computed(function() {
   //   let token = Ember.$('meta[name="csrf-token"]').attr('content');
