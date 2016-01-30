d00f78704bc84268fd4dfe5568cd20665418250c
Ian Nguyen <iannknguyen@gmail.com>
2016-01-29 17:05:38 -0800

    test api setup

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -4,8 +4,8 @@ import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
 export default DS.RESTAdapter.extend(DataAdapterMixin, {
   authorizer: 'authorizer:devise',
-  headers: Ember.computed(function() {
-    let token = Ember.$('meta[name="csrf-token"]').attr('content');
-    return {"X-CSRF-Token": token};
-  }),
+  // headers: Ember.computed(function() {
+  //   let token = Ember.$('meta[name="csrf-token"]').attr('content');
+  //   return {"X-CSRF-Token": token};
+  // }),
 });
