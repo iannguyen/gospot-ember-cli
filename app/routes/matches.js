6111a05f9f6a24c939c042181eda98262898adc8
Ian Nguyen <iannknguyen@gmail.com>
2016-01-30 19:57:11 -0800

    testing unauth route

--- app/routes/matches.js
+++ app/routes/matches.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-route-mixin';
 
-export default Ember.Route.extend(AuthenticatedRouteMixin, {
+export default Ember.Route.extend(UnauthenticatedRouteMixin, {
   model() {
     return this.store.findAll('match');
   },
