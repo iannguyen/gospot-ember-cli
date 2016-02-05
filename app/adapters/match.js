d324d6ca467bd9321c473ccd9960254833ead4fc
Ian Nguyen <iannknguyen@gmail.com>
2016-02-04 21:32:24 -0800

    add custom buildURL for matches, to reduce load time on home page

--- app/adapters/match.js
+++ app/adapters/match.js
@@ -2,4 +2,13 @@ import ApplicationAdapter from './application';
 
 export default ApplicationAdapter.extend({
   namespace: 'api',
+  buildURL(modelName, id, snapshot, requestType, query) {
+    if (requestType === 'queryRecord') {
+      let url = this._buildURL(modelName, query.id);
+      delete query.id;
+      return url;
+    } else {
+      return this._super(modelName, id, snapshot, requestType, query);
+    }
+  }
 });
